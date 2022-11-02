be932ae0ec767673419e378b72146c744dd737ea
Felix <barosiofelix@gmail.com>
2022-11-02 16:16:49 +0300

    Completed Assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+  # grab entire document from the webpage
+  def get_page
+    doc = Nokogiri::HTML(URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    #binding.pry
+  end
+
+  # grab html elements containing a certain course and return collection of nokogiri xml elements
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  # instantiate course objects and give each course the correct attribute scraped from the page
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+  # iterate over all courses and print out a list of all course offerings
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -17,6 +39,12 @@ class Scraper
   end
   
 end
+
+Scraper.new.print_courses
+
+
+# binding.pry
+
 
 
 
